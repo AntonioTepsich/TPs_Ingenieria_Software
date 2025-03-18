7b85437ff36b74a5cddfef333ba2af58341c4254
castillocande <castilloc@udesa.edu.ar>
2025-03-18 11:47:51 -0300

    cambios en los nombres para tener coherencia

--- TP1/Palet.hs
+++ TP1/Palet.hs
@@ -1,13 +1,21 @@
-module Palet ( Palet, newP, destinationP, netP )
-  where
+module Palet ( Palet, newP, destinationP, netP ) -- C: declaramos el módulo
+  where -- C: la lista dice a qué funciones pueden acceder otros módulos
 
 data Palet = Pal String Int deriving (Eq, Show)
+-- C: define el tipo de dato Palet 
+-- C: el String es la ciudad destino, el Int el peso en toneladas
+-- C: comparar palets (Eq) y mostrarlos como texto (Show)
 
+-- C: comienzan las funciones:
 newP :: String -> Int -> Palet   -- construye un Palet dada una ciudad de destino y un peso en toneladas
+-- C: recibe el string y el int, y devuelve el palet creado
+-- C: newP city load = Pal city load me lo corrige vs code
+newP = Pal
+
 destinationP :: Palet -> String  -- responde la ciudad destino del palet
+-- C: dado un palet, devuelve la ciudad
+destinationP (Pal city _) = city
+
 netP :: Palet -> Int             -- responde el peso en toneladas del palet
-
-newP destino peso = Pal destino peso
-destinationP (Pal destino _) = destino
-netP (Pal _ peso) = peso
-
+-- C: dado un palet, devuelve el peso
+netP (Pal _ load) = load
