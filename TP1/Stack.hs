0407c4d40c3134254966bba7acda5d32b65ab992
Antonio Tepsich <104152654+AntonioTepsich@users.noreply.github.com>
2025-03-23 12:10:30 -0300

    final updates

--- TP1/Stack.hs
+++ TP1/Stack.hs
@@ -8,7 +8,7 @@ data Stack = Sta [ Palet ] Int deriving (Eq, Show)
 
 
 newS :: Int -> Stack                      -- construye una Pila con la capacidad indicada 
-newS cap  | cap < 0 = error "La capacidad de la pila no puede ser negativa!!"
+newS cap  | cap <= 0 = error "La capacidad de la pila debe ser mayor a cero!"
           | otherwise = Sta [] cap
 
 freeCellsS :: Stack -> Int                -- responde la celdas disponibles en la pila
@@ -26,14 +26,16 @@ netS (Sta (x:xs) cap) = netP x + netS (Sta xs cap)
 
 holdsS :: Stack -> Palet -> Route -> Bool -- indica si la pila puede aceptar el palet considerando las ciudades en la ruta
 holdsS (Sta palets cap) pal route = 
-  (freeCellsS (Sta palets cap) > 0)           -- Que haya lugar en la pila 
+  inRouteR route (destinationP pal)           -- Que la ciudad destino esté en la ruta
+  && (freeCellsS (Sta palets cap) > 0)       -- Que haya lugar en la pila 
   && (netS (Sta palets cap) + netP pal <= 10) -- Que el peso neto de la pila mas el peso del palet no supere los 10
-  && (palets == [] || not (inOrderR route (destinationP (head palets)) (destinationP pal))) -- Que el palet a apilar no tenga destino antes que el tope de la pila
+  && (palets == [] || not (inOrderR route (destinationP pal) (destinationP (head palets)))) -- Que el palet a apilar no tenga destino posterior al tope de la pila
   
 
 
 popS :: Stack -> String -> Stack          -- quita del tope los paletes con destino en la ciudad indicada
 popS (Sta [] cap) _ = Sta [] cap
 popS (Sta (x:xs) cap) ciudad | destinationP x == ciudad = popS (Sta xs cap) ciudad  -- Si el destino del palet es la ciudad, lo saco
-                             --TODO: VERIFICAR SI NO VA UN ERROR
-                             | otherwise = Sta (x:xs) cap                          -- Si no, lo dejo 
+                             | otherwise = Sta (x : paletsRestantes) cap             -- Si no, lo dejo y sigo procesando el resto
+                             where
+                                Sta paletsRestantes _ = popS (Sta xs cap) ciudad
--- TP1/Tests.hs
+++ TP1/Tests.hs
@@ -4,7 +4,7 @@ import System.IO.Unsafe
 import Palet
 import Route
 import Stack
--- import Truck
+import Truck
 
 testF :: Show a => a -> Bool
 testF action = unsafePerformIO $ do
@@ -21,26 +21,55 @@ main :: IO ()
 main = do
   let 
     testPalet = [ 
-        ("-- No acepta peso negativo", testF (newP "roma" (-1))), 
-        ("-- No acepta ciudad vacía", testF (newP "" 1))
+        ("-- No acepta peso negativo", testF (newP "miami" (-1))), 
+        ("-- No acepta ciudad vacía", testF (newP "" 1)),
+        ("-- Obtiene destino correcto", destinationP (newP "praga" 3) == "praga"),
+        ("-- Obtiene peso neto correcto", netP (newP "parana" 4) == 4)
       ]
 
-    longRoute = newR ["roma", "paris", "mdq", "berna"]
-    palet1 = newP "roma" 1
-    palet2 = newP "paris" 1
-    palet3 = newP "mdq" 1
+    longRoute = newR ["miami", "praga", "parana", "japon"]
+    palet1 = newP "miami" 1
+    palet2 = newP "praga" 1
+    palet3 = newP "parana" 1
     arrayPalets = [palet1, palet2, palet3]
     
     testRoute =[ 
-        ("-- No acepta lista vacía",       testF (newR [])),
-        ("-- No acepta lista de una ciudad", testF (newR ["roma"])),
-        ("-- No acepta ciudad no en la ruta", testF (inRouteR longRoute "berna") == False),
-        ("-- Ciudad no en la ruta", testF (inOrderR longRoute "md" "mdq"))
+        ("-- No acepta lista vacía", testF (newR [])),
+        ("-- Acepta lista de una ciudad", testF (newR ["miami"]) == False),
+        ("-- Ciudad no en la ruta", testF (inOrderR longRoute "md" "parana")),
+        ("-- Ciudad está en la ruta", inRouteR longRoute "miami"),
+        ("-- Carga palet en orden correcto", (netT (loadT (loadT (newT 2 3 longRoute) palet2) palet1)) == 2),
+        ("-- Ciudad inexistente en orden ruta", testF (inOrderR longRoute "miami" "madrid")),
+        ("-- Orden correcto de ciudades", (inOrderR longRoute "miami" "praga") == True),
+        ("-- Orden incorrecto de ciudades", (inOrderR longRoute "praga" "miami") == False)
       ]
 
     testStack = [
         ("-- No acepta pila de 0 celdas", testF (newS 0)),
-        ("-- No acepta palet en pila llena", testF (stackS (newS 1) palet1))
+        ("-- No acepta palet en pila llena", testF (stackS (newS 0) palet1)),
+        ("-- Celdas libres correctas", freeCellsS (newS 3) == 3),
+        ("-- Peso neto de pila vacía", netS (newS 3) == 0),
+        ("-- Peso neto con palets", netS (stackS (stackS (newS 3) palet1) palet2) == 2),
+        ("-- HoldsS verifica orden correcto", holdsS (newS 3) palet1 longRoute),
+        ("-- HoldsS verifica peso máximo",  (holdsS (newS 3) (newP "miami" 18) longRoute) == False),
+        ("-- HoldsS verifica si camion va a esa ciudad",  (holdsS (newS 3) (newP "miami" 18) longRoute) == False),
+        ("-- PopS elimina palets correctamente", popS (stackS (newS 3) palet1) "miami" == newS 3),
+        ("-- Stack no acepta palet destino posterior", holdsS (stackS (newS 2) palet2) palet1 longRoute == False)
+      ]
+
+    testTruck = [
+        ("-- No acepta bahías negativas", testF (newT (-1) 3 longRoute)),
+        ("-- Celdas libres correctas", freeCellsT (newT 2 3 longRoute) == 6),
+        ("-- Peso neto de camión vacío", netT (newT 2 3 longRoute) == 0),
+        ("-- Carga y descarga palet correctamente", netT (unloadT (loadT (newT 2 3 longRoute) palet1) "miami") == 0),
+        ("-- Peso neto con palets", netT (loadT (loadT (newT 2 3 longRoute) palet1) palet2) == 2),
+        ("-- Carga palet en orden correcto", (netT (loadT (loadT (newT 2 3 longRoute) palet2) palet1)) == 2),
+        ("-- Descarga parcial de palets", netT (unloadT (loadT (loadT (newT 2 3 longRoute) palet1) palet2) "miami") == 1),
+        ("-- Camión lleno exactamente en todas sus bahías", freeCellsT (foldl loadT (newT 2 2 longRoute) [palet1, palet2, palet3, newP "parana" 1]) == 0),
+        ("-- Descarga parcial, peso restante correcto", netT (unloadT (foldl loadT (newT 2 3 longRoute) [palet1, palet2, palet3]) "praga") == 2),
+        ("-- Descarga en ciudad sin palets no afecta peso", netT (unloadT (loadT (newT 1 3 longRoute) palet1) "japon") == netT (loadT (newT 1 3 longRoute) palet1)),
+        ("-- No acepta carga cuando no quedan bahías disponibles", (testF (loadT (newT 0 3 longRoute) palet1)) == False),     --este test falla por terminal pero no en el test ya que testF no puede evaluar la excepción, por eso puse False. Pero en realidad tira excepción
+        ("-- No acepta carga palet destino fuera de ruta", testF (loadT (newT 1 3 longRoute) (newP "barcelona" 1)) == False)  --este test falla por terminal pero no en el test ya que testF no puede evaluar la excepción, por eso puse False. Pero en realidad tira excepción
       ]
     
   putStrLn "---Test Palet---"
@@ -49,9 +78,9 @@ main = do
   mapM_ printTest testRoute
   putStrLn "---Test Stack---"
   mapM_ printTest testStack
+  putStrLn "---Test Truck---"
+  mapM_ printTest testTruck
 
-
-    
 printTest :: (String, Bool) -> IO ()
 printTest (desc, ok) = putStrLn $ desc ++ ": " ++ if ok then "PASS" else "FAIL"
 
